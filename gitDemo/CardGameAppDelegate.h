4fdcfcecd73dabc6b856fc7a12344fa29025ed6b
kysd <kysd@kysddeMac-mini.local>
2014-08-21 09:03:38 +0800

    Initial Commit

---
+++ gitDemo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  gitDemo
+//
+//  Created by kysd on 14-8-21.
+//  Copyright (c) 2014年 Beijing Fast times Technology Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ gitDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  gitDemo
+//
+//  Created by kysd on 14-8-21.
+//  Copyright (c) 2014年 Beijing Fast times Technology Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CardGameAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CardGameAppDelegate class]));
+    }
+}
